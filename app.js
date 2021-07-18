a7ce79c77a49c7d3dcbd635dfa8f2585df383413
SeniolisFrame <frame2841@gmail.com>
2021-07-18 16:09:22 +0700

    test

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((err, req, res, next) => {
     })
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 443;
 
 app.listen(port,() => {
     console.log("server start")
