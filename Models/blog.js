e0480dabca17ca1325e4509a51aca1d2b8159c71
Seniolis <56755424+SeniolisFrame@users.noreply.github.com>
2021-07-29 22:28:12 +0700

    Update blog.js

--- Models/blog.js
+++ Models/blog.js
@@ -12,16 +12,6 @@ const blogSchema = new mongoose.Schema({
   },{ versionKey: false }
 );
 
-const commentSchema = new mongoose.Schema({
-    _id : mongoose.Schema.ObjectId,
-    blog_id : mongoose.Schema.ObjectId,
-    comment : String,
-    writeDate : {
-      type : Date,
-      default : Date.now
-    }
-  },
-  { versionKey : false }
-);
+
 module.exports = mongoose.model("Comment",commentSchema);
-module.exports = mongoose.model("Blog", blogSchema);
+
